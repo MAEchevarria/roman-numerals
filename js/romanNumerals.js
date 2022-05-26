7da582da0737109eed9316b11084fe74df1e9c09
Michael Echevarria <michael.echevarria@live.com>
2022-05-25 23:26:11 -0500

    updated code for js and py

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,25 @@
 exports.toRoman = function(num) {
-
+    let romanNum = "";
+    let obj = {
+        "M": 1000,
+        "CM": 900,
+        "D": 500,
+        "CD": 400,
+        "C": 100,
+        "XC": 90,
+        "L": 50,
+        "XL": 40,
+        "X": 10,
+        "IX": 9,
+        "V": 5,
+        "IV": 4,
+        "I": 1,  
+    };
+    for (let key in obj){
+        while (num >= obj[key]){
+            romanNum += key
+            num -= obj[key];
+        }
+    }
+    return romanNum;
 };
